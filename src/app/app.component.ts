7e103f5f4cd2d8ba27a2e812bde908dd9b32addb
Serhat Sait <serhat.pekediz@orgino.com.tr>
2018-08-27 14:32:50 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'AngularDersleri2';
+  title = 'Serhat Sait Pekediz ile Angular5 Derslerine Hoşgeldiniz!';
 }
